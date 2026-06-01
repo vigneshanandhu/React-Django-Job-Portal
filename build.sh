6172bc6fa4e7ef58b323aa0d3c14631d86ab965f
vigneshanandhu <vigneshanandhu2003@gmail.com>
2026-06-01 20:58:35 +0530

    commit build.sh

--- build.sh
+++ build.sh
@@ -7,5 +7,5 @@ python manage.py collectstatic --noinput
 python manage.py migrate
 
 if [[ $CREATE_SUPERUSER == "True" ]]; then
-    python manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL --password $DJANGO_SUPERUSER_PASSWORD  
+    python manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL 
 fi
